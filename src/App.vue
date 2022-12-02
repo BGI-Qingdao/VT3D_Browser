22e61164e95bcde77108a30ea9f5e3b99f7707ed
guolidong <guolidong@genomics.cn>
2022-12-02 16:15:29 +0800

    let browser accept atlas parameter

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div id="app">
     <header>
-      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" active-text-color="#409eff" style="margin: 0px;">
-        <el-menu-item index="1">Configure</el-menu-item>
-        <el-menu-item index="2" disabled >Browser</el-menu-item>
-      </el-menu>
     </header>
     <article>
-        <component :G_Atlas='G_Atlas' @browse='BrowseURL' v-bind:is="selected"></component>
+        <component  :G_Atlas='G_Atlas' v-bind:is="selected"></component>
     </article>
     <footer>
       <hr>
@@ -21,18 +17,18 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
+
 Vue.use(Element, { locale })
 import 'element-theme-dark';
 
-import Home from "./Home.vue";
+import Home from "./Home.vue"
 import Viewer from "./Viewer.vue"
 
 export default {
   data() {
     return {
       G_Atlas:'',
-      activeIndex: '1',
-      selected:"Home"
+      selected:"Home",
     }
   },
   components:{
@@ -40,27 +36,33 @@ export default {
      Viewer,
   },
   methods: {
-    handleSelect(key, keyPath) {
-        this.activeIndex = key;
-        if ( key == "1")
-        {
-            this.selected = "Home";
-        }
-        else if ( key == "2")
-        {
-            this.selected = "Viewer";
-        }
-        else
-        {
-            this.selected = "Home";
-        }
-    },
-    BrowseURL(atlas){
-        this.G_Atlas = atlas;
-        this.$nextTick(() => {
-            this.handleSelect("2",["2"]);
-        });
-    },
+      InitSummary(jsondata){
+          console.log("save summary");
+          this.G_Atlas['summary'] = jsondata;
+          this.$nextTick(() => {
+              console.log("start browser");
+              this.selected = "Viewer";
+          });
+      },
+  },
+  mounted(){
+    var uri = window.location.search.substring(1);
+    var params = new URLSearchParams(uri);
+    var baseurl=params.get("atlas");
+    if( baseurl == null ) baseurl = ""
+    var atlas = {}
+    atlas['main_url']    = baseurl;
+    atlas['summary_url'] = baseurl + '/summary.json';
+    atlas['gene_url']    = baseurl + '/gene.json';
+    atlas['meshes_url']  = baseurl + '/meshes.json';
+    atlas['anno_url']    = baseurl + '/Anno';
+    atlas['genes_url']   = baseurl + '/Gene';
+    this.G_Atlas = atlas;
+    var self = this;
+    $.getJSON(atlas['summary_url'],function(_data) {
+        console.log("summary loaded");
+        self.InitSummary(_data);
+    });
   },
 }
 </script>
--- src/Home.vue
+++ src/Home.vue
@@ -1,36 +1,6 @@
 <template>
 <div>
-  <div>
-     <el-row>
-         <el-col :span="24" >
-             <p>Please type the address of VT3D atlas</p>
-         </el-col >
-     </el-row>
-     <el-row>
-         <el-col :span="4" >
-             <p> Atlas:</p>
-         </el-col >
-         <el-col :span="16" >
-             <el-input placeholder="http://127.0.0.1:8010" v-model="input_url"></el-input>
-         </el-col >
-         <el-col :span="4" >
-             <el-button type="primary"  @click='ConnectAtlas' >Connect</el-button>
-         </el-col >
-     </el-row>
-  </div>
-  <div v-show="summary_div" style="background-color: #777">
-     <el-row>
-         <el-col :span="4" >
-             <p> Summary:</p>
-         </el-col >
-         <el-col :span="16" >
-              <p> Total number of cells: {{num_of_cells}}. Total number of genes: {{num_of_genes}}. </p>
-         </el-col>
-         <el-col :span="4" >
-             <el-button type="success"  @click='BrowseAtlas' >Browse</el-button>
-         </el-col>
-     </el-row>
-  </div>
+    <p> Preparing window now ... </p>
 </div>
 </template>
 
@@ -38,44 +8,9 @@
 export default {
     props: ['G_Atlas'],
     data() {
-        return {
-            input_url : "http://127.0.0.1:8010",
-            summary_div : false,
-            num_of_cells: 0,
-            num_of_genes: 0,
-            atlas : {}
-        }
+        return {}
     },
     methods: {
-        InitAtlas(){
-            this.atlas = {}
-            this.atlas['main_url'] = this.input_url;
-            this.atlas['summary_url'] = this.input_url + '/summary.json';
-            this.atlas['gene_url'] = this.input_url + '/gene.json';
-            this.atlas['meshes_url'] = this.input_url + '/meshes.json';
-            this.atlas['anno_url'] = this.input_url + '/Anno';
-            this.atlas['genes_url'] = this.input_url + '/Gene';
-        },
-        InitSummary(jsondata){
-            this.atlas['summary'] = jsondata;
-            this.num_of_cells = jsondata['total_cell'];
-            this.num_of_genes = jsondata['total_gene'];
-            this.summary_div = true;
-        },
-        ConnectAtlas() {
-            this.InitAtlas()
-            var self = this;
-            $.getJSON(this.atlas['summary_url'],function(_data) {
-                console.log("summary loaded");
-                self.InitSummary(_data);
-            });
-        },
-        BrowseAtlas() {
-            console.log(this.input_url);
-            this.$emit('browse',this.atlas);
-        },
-    },
-    mounted() {
-    },
+    }
 };
 </script>
